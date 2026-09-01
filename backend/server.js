e1c6988c96ac33c5239af333e8af9c040f2de7a7
chawanvikash <chawanvikash30@gmail.com>
2026-09-01 23:07:08 +0530

    fixed vercel-json1

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const wrapAsync = require("./utils/wrapAsync");
 
 const allowedOrigins = [
     'http://localhost:5173', // Your local Vite React port (change if it's 3000)
-    'https://auto-form-correct.vercel.app/' // Replace with your actual Vercel URL
+    'https://auto-form-correct.vercel.app' // Replace with your actual Vercel URL
 ];
 
 app.use(cors({
