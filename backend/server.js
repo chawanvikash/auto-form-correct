e357547e441b7f8a1f9e504e3bf6e8bbd95b23f9
chawanvikash <chawanvikash30@gmail.com>
2026-09-01 22:55:01 +0530

    fixed cors2

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const ExpressError = require("./utils/ExpressError");
 const wrapAsync = require("./utils/wrapAsync");
 
 // Middleware
-const cors = require('cors');
+
 const allowedOrigins = [
     'http://localhost:5173', // Your local Vite React port (change if it's 3000)
     'https://auto-form-correct.vercel.app/' // Replace with your actual Vercel URL
